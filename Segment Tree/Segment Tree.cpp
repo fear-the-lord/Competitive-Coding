30b083c98c586c7284d53d803a57326aa66f64e5
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2020-09-07 19:30:46 +0530

    Add files via upload

--- Segment Tree/Segment Tree.cpp
+++ Segment Tree/Segment Tree.cpp
@@ -40,6 +40,44 @@ int query(int *tree, int s, int e, int qs, int qe, int index) {
 	return min(left, right);
 }
 
+void point_update(int *tree, int s, int e, int i, int increment, int index) {
+	
+	// Where the index does not lie within the range 
+	if(i < s || i > e) {
+		return; 
+	}
+	
+	// Leaf nodes 
+	if(s == e) {
+		tree[index] += increment;
+		return; 
+	}
+	
+	int mid = (s + e) / 2; 
+	point_update(tree, s, mid, i, increment, 2 * index);
+	point_update(tree, mid + 1, e, i, increment, 2 * index + 1); 
+	tree[index] = min(tree[2 * index], tree[2 * index + 1]); 
+}
+
+void range_update(int *tree, int s, int e, int qs, int qe, int increment, int index) {
+	
+	// Out of bounds 
+	if(qs > e || qe < s) {
+		return;
+	}
+	
+	// Leaf nodes 
+	if(s == e) {
+		tree[index] += increment;
+		return; 
+	}
+	
+	int mid = (s + e) / 2; 
+	range_update(tree, s, mid, qs, qe, increment, 2 * index); 
+	range_update(tree, mid + 1, e, qs, qe, increment, 2 * index + 1); 
+	tree[index] = min(tree[2 * index], tree[2 * index + 1]);
+}
+
 int main() {
 	
 	int arr[] = {1, 3, 2, -5, 6, 4};
@@ -51,7 +89,10 @@ int main() {
 //	for(int i = 1; i <= 13; i++) {
 //		cout << tree[i] << " ";
 //	} 
-
+	
+	point_update(tree, 0, n - 1, 3, 10, 1);
+	range_update(tree, 0, n - 1, 3, 5, 10, 1);
+	
 	int q; 
 	cin >> q; 
 	while(q--) {
